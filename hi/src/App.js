283c612c8ffe3ec2cdecf18400ea5537e04071f2
rnr9928 <rnr99281@gmail.com>
2023-07-19 21:43:48 +0900

    콜라 바운ㅅ

--- hi/src/App.js
+++ hi/src/App.js
@@ -1,10 +1,27 @@
-
+// src/App.js
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleColaClick = () => {
+    console.log("클릭됨?")
+    setIsActive(true);
+
+    setTimeout(() => {
+      setIsActive(false);
+    }, 1000);
+  };
+
   return (
-    <div className="App">
-     마지막 테스트2
+    <div className="Card">
+      <img
+        className={`Cola${isActive ? ' active' : ''}`}
+        src="img/cola.png"
+        alt="콜라 이미지"
+        onClick={handleColaClick}
+      />
     </div>
   );
 }
